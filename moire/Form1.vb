1d053dff511dceec101b47cc72c3f414e4fdc559
knappyflash <knappyflash2@gmail.com>
2025-05-19 06:35:19 -0700

    Added Events to ioListener Class.

--- moire/Form1.vb
+++ moire/Form1.vb
@@ -3,10 +3,14 @@
 Public Class Form1
 
     Private _MyScreenCapture As SCREEN_CAPTURE_FORM
-    Dim MyioListener As New ioListener
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        MyioListener.Timer1.Start()
+        ioListener.Timer1.Start()
+
+        AddHandler ioListener.Released_Escape, AddressOf QuickTest1
+        AddHandler ioListener.Released_PrintScreen, AddressOf QuickTest2
+        AddHandler ioListener.Released_LMouse, AddressOf QuickTest3
+        AddHandler ioListener.Released_RMouse, AddressOf QuickTest4
 
         '_MyScreenCapture = New SCREEN_CAPTURE_FORM
         'MAIN_APP_FORM.TopLevel = False
@@ -18,6 +22,22 @@ Public Class Form1
     End Sub
     Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         End
+    End Sub
+
+    Private Sub QuickTest1()
+        Console.WriteLine($"{Now} Escape Released!")
+    End Sub
+
+    Private Sub QuickTest2()
+        Console.WriteLine($"{Now} PrintScreen Released!")
+    End Sub
+
+    Private Sub QuickTest3()
+        Console.WriteLine($"{Now} LMouse Released!")
+    End Sub
+
+    Private Sub QuickTest4()
+        Console.WriteLine($"{Now} RMouse Released!")
     End Sub
 
 End Class
--- moire/ioListener.vb
+++ moire/ioListener.vb
@@ -6,34 +6,56 @@
 
 Public Class ioListener
 
-
-
     Private Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal vKey As Integer) As Short
+
+    Public Event Released_Escape()
+    Public Event Released_PrintScreen()
+    Public Event Released_LMouse()
+    Public Event Released_RMouse()
 
     Private Sub ioListener_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
 
-    Private EscapeWasPressed As Boolean = False
-    Private PrintScreenWasPressed As Boolean = False
+    Private EscapeStillDown As Boolean = False
+    Private PrintScreenStillDown As Boolean = False
+    Private LeftMouseStillDown As Boolean = False
+    Private RightMouseStillDown As Boolean = False
+
+    Public Sub Start_Timer()
+        Timer1.Start()
+    End Sub
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
         Dim EscapeCurrentState As Boolean = GetAsyncKeyState(Keys.Escape) <> 0
         Dim PrintScreenCurrentState As Boolean = GetAsyncKeyState(Keys.PrintScreen) <> 0
+        Dim LeftMouseCurrentState As Boolean = GetAsyncKeyState(Keys.LButton) <> 0
+        Dim RightMouseCurrentState As Boolean = GetAsyncKeyState(Keys.RButton) <> 0
 
-        If EscapeCurrentState Then EscapeWasPressed = True
-        If PrintScreenCurrentState Then PrintScreenWasPressed = True
+        If EscapeCurrentState Then EscapeStillDown = True
+        If PrintScreenCurrentState Then PrintScreenStillDown = True
+        If LeftMouseCurrentState Then LeftMouseStillDown = True
+        If RightMouseCurrentState Then RightMouseStillDown = True
 
-
-        If EscapeWasPressed And Not EscapeCurrentState Then
-            Console.WriteLine($"{Now} Escape Was Realesed!")
-            EscapeWasPressed = False
+        If EscapeStillDown And Not EscapeCurrentState Then
+            RaiseEvent Released_Escape()
+            EscapeStillDown = False
         End If
 
-        If PrintScreenWasPressed And Not PrintScreenCurrentState Then
-            Console.WriteLine($"{Now} PrintScreen Was Realesed!")
-            PrintScreenWasPressed = False
+        If PrintScreenStillDown And Not PrintScreenCurrentState Then
+            RaiseEvent Released_PrintScreen()
+            PrintScreenStillDown = False
+        End If
+
+        If LeftMouseStillDown And Not LeftMouseCurrentState Then
+            RaiseEvent Released_LMouse()
+            LeftMouseStillDown = False
+        End If
+
+        If RightMouseStillDown And Not RightMouseCurrentState Then
+            RaiseEvent Released_RMouse()
+            RightMouseStillDown = False
         End If
 
     End Sub
